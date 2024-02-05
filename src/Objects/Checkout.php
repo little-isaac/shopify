9d3a884814759cef8703dadbc31ea4da0ce213bb
VrajPatel-1511 <159014228+VrajPatel-1511@users.noreply.github.com>
2024-02-05 18:12:44 +0530

    Update Checkout.php

--- src/Objects/Checkout.php
+++ src/Objects/Checkout.php
@@ -1,14 +1,18 @@
 <?php
+
 /*
- * 
+ * this file will create shopify object class
  */
-namespace App\Classes\Shopify;
+
+namespace Shopify\Objects;
+
 use Shopify\ShopifyObject;
+
 class Checkout extends ShopifyObject {
-    protected $SINGLE = "admin/checkouts/{id}.json";
-    protected $UPDATE = "admin/checkouts/{id}.json";
-    protected $ARRAY_NAME = "checkout";
-    protected $COMPLETE_CHECKOUT = "admin/checkout/{id}/complete.json";
-    protected $GET_SHIPPING_RATES = "admin/checkout/{id}/shipping_rates.json";
-    protected $is_having_primary_key = true;
+    protected $SINGLE = "admin/checkouts/{id}.json"; // get url for single
+    protected $UPDATE = "admin/checkouts/{id}.json"; // url for update single
+    protected $ARRAY_NAME = "checkout"; // array name in update/create
+    protected $COMPLETE_CHECKOUT = "admin/checkout/{id}/complete.json"; // complete a checkout
+    protected $GET_SHIPPING_RATES = "admin/checkout/{id}/shipping_rates.json"; // get shipping rates from a checkout
+    protected $is_having_primary_key = true; // get is primary key in object or not
 }
